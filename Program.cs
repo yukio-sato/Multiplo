9b5a1dea47ab5df49dd7e8fd6bbf3c4043eddd97
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-10-21 22:44:25 -0300

    Multiplo

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+string frase = "- - Multiplo - -\n";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+Console.ForegroundColor = ConsoleColor.Yellow;
+frase = "Escreva um número para se dividir: ";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.Cyan;
+frase = "Escreva um número para dividir o numero escolhido: ";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+int numero2 = Convert.ToInt32(Console.ReadLine());
+if (numero % numero2 != 1)
+{
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+frase = $"{numero} é multiplo\n";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+}
+else if (numero % numero2 >= 1)
+Console.ForegroundColor = ConsoleColor.Red;
+frase = $"{numero} não é multiplo\n";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+
+Console.ResetColor();
